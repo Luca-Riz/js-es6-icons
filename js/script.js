4d65dc84588ea228ccce02a32c8f8994229850e6
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-18 12:00:10 +0200

    add non funziona

--- js/script.js
+++ js/script.js
@@ -19,24 +19,29 @@ const icons = [
 
 incons.forEach((element) => {
     const {name, prefix, type, family} = element;
+    // console.log(type);
     document.getElementById('boxIcon').innerHTML += (
+        
         `
-        <div class="col-2">
-            <div class="ret_animal bg-light d-flex justify-content-center rounded-3 my-4">
-                <div><i class="${family} ${prefix}${name}"></i></div>
-                <div class="name text-uppercase">${name}</div>
-            </div>             
+        <div class="row">
+            <div class="col-2">
+                <div class="ret_animal bg-light d-flex justify-content-center rounded-3 my-4">
+                    <div><i class="${family} ${prefix}${name}"></i></div>
+                    <div class="name text-uppercase">${name}</div>
+                </div>             
+            </div>
         </div>  
         `
+
     );
 });
 
 
 
 
-<div class="col-2">
-    <div class="ret_animal bg-light d-flex justify-content-center rounded-3 my-4">
-        <div><i class="fas fa-cat"></i></div>
-        <div class="name text-uppercase">Cats</div>
-    </div>             
-</div>
+// {/* <div class="col-2">
+//     <div class="ret_animal bg-light d-flex justify-content-center rounded-3 my-4">
+//         <div><i class="fas fa-cat"></i></div>
+//         <div class="name text-uppercase">Cats</div>
+//     </div>             
+// </div> */}

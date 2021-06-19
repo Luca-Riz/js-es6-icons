044d8a885cb5fbf6e242201e686248fbe307f15f
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-19 14:20:49 +0200

    add function print

--- js/script.js
+++ js/script.js
@@ -17,20 +17,29 @@ const icons = [
     {name: 'user-secret', prefix: 'fa-',type: 'user',family: 'fas', },
 ];
 
-icons.forEach((element) => {
-    const {name, prefix, type, family} = element;
-    console.log(type);
-    document.getElementById('boxIcon').innerHTML += (
-        
-        `
-        <div class="col-2">
-            <div class="ret_animal bg-light d-flex flex-column justify-content-center align-items-center rounded-3 my-4">
-                <div><i class="${family} ${prefix}${name} ${type} fs-1"></i></div>
-                <div class="name text-uppercase fs-5">${name}</div>
-            </div>             
-        </div>
-        `
-    );
-});
+print(icons);
+
+/*--------- funzioni ---------*/
+function print(array){
+    array.forEach((element) => {
+        const {name, prefix, type, family} = element;
+        console.log(type);
+        document.getElementById('boxIcon').innerHTML += (        
+            `
+            <div class="col-2">
+                <div class="ret_animal bg-light d-flex flex-column justify-content-center align-items-center rounded-3 my-4">
+                    <div><i class="${family} ${prefix}${name} ${type} fs-1"></i></div>
+                    <div class="name text-uppercase fs-5">${name}</div>
+                </div>             
+            </div>
+            `
+        );
+    });
+
+}
+
+
+
+
 
 // Milestone 2 Coloriamo le icone per tipo
